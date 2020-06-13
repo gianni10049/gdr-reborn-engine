d071d6af2dda2e6ee4da60756d88a03c035b1b32
749666 <gianni95@gmail.com>
2020-06-13 04:08:08 +0200

    Nuovo db importato

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Giu 10, 2020 alle 01:31
+-- Generation Time: Giu 13, 2020 alle 04:07
 -- Versione del server: 5.6.33-log
 -- PHP Version: 5.3.10
 
@@ -23,6 +23,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fingerprint` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_active` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `config`
 --
 
@@ -30,19 +47,19 @@ CREATE TABLE IF NOT EXISTS `config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `default` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `default` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `editable` tinyint(1) NOT NULL DEFAULT '1',
   `global` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 --
 -- Dump dei dati per la tabella `config`
 --
 
-REPLACE INTO `config` (`id`, `type`, `name`, `default`, `value`, `description`, `editable`, `global`) VALUES
+INSERT INTO `config` (`id`, `type`, `name`, `default`, `value`, `description`, `editable`, `global`) VALUES
 (1, 'Int', 'session_timeout', '360', NULL, NULL, 1, 1),
 (2, 'Int', 'login_max_attempts', '10', NULL, NULL, 1, 1),
 (3, 'Int', 'cookie_expire', '100000', NULL, NULL, 1, 1),
@@ -55,7 +72,9 @@ REPLACE INTO `config` (`id`, `type`, `name`, `default`, `value`, `description`, 
 (10, 'Int', 'email_min', '8', NULL, NULL, 1, 1),
 (11, 'Int', 'email_max', '16', NULL, NULL, 1, 1),
 (12, 'Int', 'cookie_lifetime', '0', NULL, NULL, 1, 1),
-(13, 'Array', 'request_allowed_methods', 'GET,POST', NULL, NULL, 1, 1);
+(13, 'Array', 'request_allowed_methods', 'GET,POST', NULL, NULL, 1, 1),
+(14, 'String', 'cryptography_key', 'V6/qcAev/bcu0nkrsgA9M/TpR/+Pxz1CdNK/U/8f77s=', NULL, NULL, 1, 1),
+(15, 'String', 'cryptography_method', 'aes-128-gcm', NULL, NULL, 1, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
